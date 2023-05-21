968e7609aa7bdf99c5c20b03d703059d3f9e7441
OmarGaafar1 <omar_gaafar989@yahoo.com>
2023-05-22 01:06:50 +0200

    Ajax done for student form

--- Higher-studies/higher_studies/api/urls.py
+++ Higher-studies/higher_studies/api/urls.py
@@ -4,8 +4,9 @@ from database.models import Student
 
 
 urlpatterns = [
-    path('students/', views.getStudents),
-    path('courses/', views.getCourses),
+    path('students/', views.getStudents),# used in Backend validation to sure that the user did not enter duplicated ID
+    path('courses/', views.getCourses, name='getCourses'),#used in  rendering courses to the form
     path('enrollments/', views.getEnrollments),
     path('Students/<int:object_id>/', views.get_object, name='get-object'),
+    path('Students/addNewStudent/', views.add_student, name='add_student'),#used in adding new student from form
 ]
--- Higher-studies/higher_studies/api/views.py
+++ Higher-studies/higher_studies/api/views.py
@@ -6,7 +6,8 @@ from .serializers import StudentSerializer, CourseSerializer, EnrollmentSerializ
 from rest_framework.views import APIView
 from rest_framework import status
 from django.http import JsonResponse
-
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 @api_view(['GET'])
 def getStudents(request):
@@ -40,4 +41,59 @@ def get_object(request, object_id):
 
     # Return the serialized object as a response
     return Response(serializer.data)
+
+@csrf_exempt
+@api_view(['POST'])
+def add_student(request):
+    if request.method == 'POST':
+        data = request.data
+       
+        if data['Gender'] == 'Male':
+            data["Gender"] = 'M'
+        else:
+            data["Gender"] = 'F'
+        if data['Active'] == 'Active':
+            data["Active"] = 'A'
+        else:
+            data["Active"] = 'I'
+        data['Date'] = data['Date'].replace("/", "-")
+        
+        serializer = StudentSerializer(data={
+            'id':data['ID'],
+            'firstName': data['fname'],
+            'lastName': data['lname'],
+            'gender': data['Gender'],
+            'status': data['Active'],
+            'dob': data['Date'],
+            'university': data['Uni'],
+            'course1': data['course_1'],
+            'course2': data['course_2'],
+            'course3': data['course_3'],
+            'department': data['Dept']
+        })
+       
+        if serializer.is_valid():
+            student = serializer.save()
+        else:
+            response_data = {'error': 'ID is aleady Existed'}
+            return JsonResponse(response_data, status=400)
+        
+        Response_Data = {'message': "Student added successfully"}
+        return JsonResponse(Response_Data)
+ 
+
+@api_view(['GET'])
+def getCourses(request):
+    courses = Course.objects.all()  # Retrieve all Course objects
+
+    serializer = CourseSerializer(courses, many=True)  # Set many=True to serialize multiple objects
+
+    return Response(serializer.data)
+
+
+
+
+
+
+
 
